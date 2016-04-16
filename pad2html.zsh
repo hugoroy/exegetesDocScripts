06b0e5c0908cd60f6ca0b93fa9cd97a73c4ffa34
Hugo Roy <hugo@fsfe.org>
2016-04-17 00:15:02 +0200

    commenting out until it works

--- pad2html.zsh
+++ pad2html.zsh
@@ -8,8 +8,8 @@
 
     pandoc -f markdown PADNUMBER -t json --self-contained -o /tmp/PADNUMBER.json --filter pandoc-citeproc  \
         && \
-    pandoc -f json /tmp/PADNUMBER.json -t json --self-contained -o /tmp/PADNUMBER.json.0 --filter ./filters/html.zsh \
-        && \
-    pandoc -f json /tmp/PADNUMBER.json.0 -t json --self-contained -o /tmp/PADNUMBER.json.1 --filter ./filters/nettoyage.zsh \
-        && \
-    pandoc -f json /tmp/PADNUMBER.json.1 -t html5 --self-contained -o DOCUMENT-TITLE.html --filter ./filters/loi.py 
+    # pandoc -f json /tmp/PADNUMBER.json -t json --self-contained -o /tmp/PADNUMBER.json.0 --filter ./filters/html.zsh \
+    #     && \
+    # pandoc -f json /tmp/PADNUMBER.json.0 -t json --self-contained -o /tmp/PADNUMBER.json.1 --filter ./filters/nettoyage.zsh \
+    #     && \
+    pandoc -f json /tmp/PADNUMBER.json.1 -t html5 --self-contained -o DOCUMENT-TITLE.html #--filter ./filters/loi.py 
