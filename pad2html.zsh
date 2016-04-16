f4cee4d35d4e1548ce65862ca2a39991d1ba9460
Hugo Roy <hugo@fsfe.org>
2016-04-17 00:10:08 +0200

    output HTML

---
+++ pad2html.zsh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+# FIXME : make get-pad work, make PADNUMBER and DOCUMENT-TITLE into variables
+
+# Get pad
+#./get-pad/get-pad.pl --options \
+#    && \ # Run pandoc' citation process 
+
+    pandoc -f markdown PADNUMBER -t json --self-contained -o /tmp/PADNUMBER.json --filter pandoc-citeproc  \
+        && \
+    pandoc -f json /tmp/PADNUMBER.json -t json --self-contained -o /tmp/PADNUMBER.json.0 --filter ./filters/html.zsh \
+        && \
+    pandoc -f json /tmp/PADNUMBER.json.0 -t json --self-contained -o /tmp/PADNUMBER.json.1 --filter ./filters/nettoyage.zsh \
+        && \
+    pandoc -f json /tmp/PADNUMBER.json.1 -t html5 --self-contained -o DOCUMENT-TITLE.html --filter ./filters/loi.py 
