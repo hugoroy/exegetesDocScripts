a44083f9f5084f4974cf672120f79b49ccfb4d80
Hugo Roy <hugo@fsfe.org>
2016-05-02 22:32:05 +0200

    make nonbreakable spaces before guillemets

--- filters/nettoyage.zsh
+++ filters/nettoyage.zsh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-sed 's/<<~/« /g' | sed 's/~>>/ »/g' | sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»"#"t":"Str","c":" »"#g' 
+sed 's/<<~/« /g' | sed 's/~>>/ »/g' | && \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»"#"t":"Str","c":" »"#g' && \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»."#"t":"Str","c":" »."#g' && \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»,"#"t":"Str","c":" »,"#g' && \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»;"#"t":"Str","c":" »;"#g' && \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»?"#"t":"Str","c":" »?"#g' && \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»!"#"t":"Str","c":" »!"#g'
