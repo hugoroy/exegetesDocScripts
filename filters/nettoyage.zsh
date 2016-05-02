3998a37b931528f0b960f8eabbefeb0f5d1d2005
Hugo Roy <hugo@fsfe.org>
2016-05-02 22:41:40 +0200

    make nonbreakable spaces before guillemets

--- filters/nettoyage.zsh
+++ filters/nettoyage.zsh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
-sed 's/<<~/« /g' | sed 's/~>>/ »/g'  \
-| sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»"#"t":"Str","c":" »"#g'  \
-| sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»."#"t":"Str","c":" »."#g'  \
-| sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»,"#"t":"Str","c":" »,"#g'  \
-| sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»;"#"t":"Str","c":" »;"#g'  \
-| sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»?"#"t":"Str","c":" »?"#g'  \
-| sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»!"#"t":"Str","c":" »!"#g'
+sed 's/<<~/« /g' | sed 's/~>>/ »/g' | \
+    sed 's#"t":"Str","c":"«"},{"t":"Space","c":\[\]#"t":"Str","c":"« "#g' | \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»"#"t":"Str","c":" »"#g' | \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»."#"t":"Str","c":" »."#g' | \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»,"#"t":"Str","c":" »,"#g' | \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»;"#"t":"Str","c":" »;"#g' | \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»?"#"t":"Str","c":" »?"#g' | \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»!"#"t":"Str","c":" »!"#g'
